2e6922f16161182d4f9b57da4d6479d6d540c250
Sam Blum <samblum17@icloud.com>
2021-01-29 13:47:33 -0600

    Initial Commit

---
+++ Project BON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project BON
+//
+//  Created by Sam Blum on 1/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project BON/Project_BONApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_BONApp.swift
+//  Project BON
+//
+//  Created by Sam Blum on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Project_BONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
