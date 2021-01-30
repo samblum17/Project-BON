b62cf79fec5f7b932f0c84d0e1ebe8d639b370e4
Sam Blum <samblum17@icloud.com>
2021-01-30 01:28:32 -0600

    Fixed bugs

--- Project BON/Help.swift
+++ Project BON/Help.swift
@@ -9,35 +9,40 @@ import SwiftUI
 import SafariServices
 
 struct Help: View {
-    @State private var showingVC = false
+    @State private var showingTable = false
+    @State private var showingProtocols = false
 
     var body: some View {
         ZStack{
             Color.black.ignoresSafeArea()
             VStack(alignment: .leading){
-                Text("Data pulled from Vanderbilt University's COVID-19 Dashboard:")
+                Text("More Info").foregroundColor(.white)
+                    .font(.largeTitle).bold()
+                    .padding(.bottom)
+                    .padding(.bottom)
+                Text("Data pulled from Vanderbilt University's Live COVID-19 Chart:")
                     .foregroundColor(.white)
                 Button(action: {
-                    self.showingVC.toggle()
+                    self.showingTable.toggle()
                 }) {
                     Text("https://docs.google.com/spreadsheets/d/1QorVReLcwOEsqDEgWhVAlIlU3zJRNwu8m975aQ8MXpE/edit#gid=693781790")
                     .foregroundColor(Color.blue)
-                }.sheet(isPresented: $showingVC, content:{
+                }.sheet(isPresented: $showingTable, content:{
                     SafariView(safariVC: SFSafariViewController(url: URL(string: "https://docs.google.com/spreadsheets/d/1QorVReLcwOEsqDEgWhVAlIlU3zJRNwu8m975aQ8MXpE/edit#gid=693781790")!), urlString: "https://docs.google.com/spreadsheets/d/1QorVReLcwOEsqDEgWhVAlIlU3zJRNwu8m975aQ8MXpE/edit#gid=693781790")
                 })
                 .padding(.bottom)
-               
-                Text("View updated campus protocols on the official Return to Campus website:").foregroundColor(.white)
+                .padding(.bottom)
+                Text("Updated campus protocols from the official Return to Campus website:").foregroundColor(.white)
                 Button(action: {
-                    self.showingVC.toggle()
+                    self.showingProtocols.toggle()
                 }) {
                     Text("https://www.vanderbilt.edu/coronavirus/")
                     .foregroundColor(Color.blue)
-                }.sheet(isPresented: $showingVC, content:{
+                }.sheet(isPresented: $showingProtocols, content:{
                     SafariView(safariVC: SFSafariViewController(url: URL(string: "https://www.vanderbilt.edu/coronavirus/")!), urlString: "https://www.vanderbilt.edu/coronavirus/")
                 })
+                Spacer()
             }.padding()
-            
         }
     }
 }
